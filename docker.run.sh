b0a20fbc68e45e2938f738505deaf8ac03cbaa82
Antoine Muller <niantoinesan@gmail.com>
2021-03-11 15:29:41 +0100

    add logger

--- docker.run.sh
+++ docker.run.sh
@@ -4,6 +4,7 @@ docker run \
     -p 3030:3030 \
     -v `pwd`/:/memodogs \
     -w /memodogs \
+    -e "RUST_LOG=debug" \
     -e "MEMODOGS_PORT=3030" \
     -it memodogs:dev-server \
     /bin/sh
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use warp::Filter;
 
 mod config;
+mod app_logger;
 
 #[tokio::main]
 async fn main() {
+    app_logger::init_logger();
     let config = config::get_config();
     let index = warp::path::end().map(|| "Hello world !");
+
+    log::info!("Server running on port {}", config.port);
     warp::serve(index).run(([0, 0, 0, 0], config.port)).await;
 }
